9eed3f8766d087acd6bba667e9ea36a14f3cbcf3
Jtejaxun-2022229 <134737462+JTejaxun-2022229@users.noreply.github.com>
2024-03-03 23:33:44 -0600

    fusion de developer a main
    * se inicia proyecto por el servidor
    
    * se crea modelo, controlador, rutas y coleccion de user
    
    * se crea controlador y rutas para auth
    
    * se crea modelo, controlador, rutas y coleccion de company(incluyendo excel)
    
    * se crea modelo, controlador, rutas y coleccion de company(incluyendo excel)

---
+++ config/server.js
@@ -0,0 +1,50 @@
+'use strict'
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+import authRoutes from '../src/auth/auth.routes.js';
+import userRoutes from '../src/user/user.routes.js';
+import companyRoutes from '../src/company/company.routes.js';
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.authPath = '/api/v1/auth'
+        this.userPath = '/api/v1/users'
+        this.companyPath = '/api/v1/companies'
+
+        this.middlewares();
+        this.conectarDB();
+        this.routes();
+    };
+
+    async conectarDB(){
+        await dbConnection();
+    };
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    };
+
+    routes(){
+        this.app.use(this.authPath, authRoutes);
+        this.app.use(this.userPath, userRoutes);
+        this.app.use(this.companyPath, companyRoutes);
+    };
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server running on port', this.port);
+        });
+    };
+};
+
+export default Server;
